cb8d4e5c1f6953c87cb208e7f7da1b2bbeef8ab4
Pol Gannau Coma <PolGannau@users.github.com>
2018-10-09 23:31:33 +0200

    First tiled

---
+++ ASSETS/gameassets/Ground.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Ground" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="goodly-2x.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="33"/>
+  <terrain name="Ice" tile="169"/>
+  <terrain name="Rock" tile="105"/>
+  <terrain name="Metal" tile="41"/>
+  <terrain name="Poisonous_Dirt" tile="97"/>
+  <terrain name="Dirt Pilon" tile="35"/>
+  <terrain name="Ice Pilon" tile="171"/>
+  <terrain name="Rock Pilon" tile="107"/>
+  <terrain name="Metal Pilon" tile="43"/>
+  <terrain name="Poisonous_Dirt Pilon" tile="99"/>
+  <terrain name="Dirt Platform" tile="21"/>
+  <terrain name="Ice Platform" tile="157"/>
+  <terrain name="Rock Platform" tile="93"/>
+  <terrain name="Metal Platform" tile="29"/>
+  <terrain name="Poisonous_Dirt platform" tile="85"/>
+  <terrain name="Dirt single" tile="1"/>
+  <terrain name="Ice Single" tile="137"/>
+  <terrain name="Rock Single" tile="73"/>
+  <terrain name="Metal Single" tile="9"/>
+  <terrain name="Poisonous_Dirt Single" tile="65"/>
+ </terraintypes>
+ <tile id="1" terrain="15,15,15,15"/>
+ <tile id="9" terrain="18,18,18,18"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,0,0,0"/>
+ <tile id="19" terrain="5,5,5,5"/>
+ <tile id="20" terrain="10,10,10,10"/>
+ <tile id="21" terrain="10,10,10,10"/>
+ <tile id="22" terrain="10,10,10,10"/>
+ <tile id="24" terrain="3,3,3,3"/>
+ <tile id="25" terrain="3,3,3,3"/>
+ <tile id="26" terrain="3,3,3,3"/>
+ <tile id="27" terrain="8,8,8,8"/>
+ <tile id="28" terrain="13,13,13,13"/>
+ <tile id="29" terrain="13,13,13,13"/>
+ <tile id="30" terrain="13,13,13,13"/>
+ <tile id="32" terrain="0,0,0,0"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="0,0,0,0"/>
+ <tile id="35" terrain="5,5,5,5"/>
+ <tile id="40" terrain="3,3,3,3"/>
+ <tile id="41" terrain="3,3,3,3"/>
+ <tile id="42" terrain="3,3,3,3"/>
+ <tile id="43" terrain="8,8,8,8"/>
+ <tile id="48" terrain="0,0,0,0"/>
+ <tile id="49" terrain="0,0,0,0"/>
+ <tile id="50" terrain="0,0,0,0"/>
+ <tile id="51" terrain="5,5,5,5"/>
+ <tile id="56" terrain="3,3,3,3"/>
+ <tile id="57" terrain="3,3,3,3"/>
+ <tile id="58" terrain="3,3,3,3"/>
+ <tile id="59" terrain="8,8,8,8"/>
+ <tile id="65" terrain="19,19,19,19"/>
+ <tile id="73" terrain="17,17,17,17"/>
+ <tile id="80" terrain="4,4,4,4"/>
+ <tile id="81" terrain="4,4,4,4"/>
+ <tile id="82" terrain="4,4,4,4"/>
+ <tile id="83" terrain="9,9,9,9"/>
+ <tile id="84" terrain="14,14,14,14"/>
+ <tile id="85" terrain="14,14,14,14"/>
+ <tile id="86" terrain="14,14,14,14"/>
+ <tile id="88" terrain="2,2,2,2"/>
+ <tile id="89" terrain="2,2,2,2"/>
+ <tile id="90" terrain="2,2,2,2"/>
+ <tile id="91" terrain="7,7,7,7"/>
+ <tile id="92" terrain="12,12,12,12"/>
+ <tile id="93" terrain="12,12,12,12"/>
+ <tile id="94" terrain="12,12,12,12"/>
+ <tile id="96" terrain="4,4,4,4"/>
+ <tile id="97" terrain="4,4,4,4"/>
+ <tile id="98" terrain="4,4,4,4"/>
+ <tile id="99" terrain="9,9,9,9"/>
+ <tile id="104" terrain="2,2,2,2"/>
+ <tile id="105" terrain="2,2,2,2"/>
+ <tile id="106" terrain="2,2,2,2"/>
+ <tile id="107" terrain="7,7,7,7"/>
+ <tile id="112" terrain="4,4,4,4"/>
+ <tile id="113" terrain="4,4,4,4"/>
+ <tile id="114" terrain="4,4,4,4"/>
+ <tile id="115" terrain="9,9,9,9"/>
+ <tile id="120" terrain="2,2,2,2"/>
+ <tile id="121" terrain="2,2,2,2"/>
+ <tile id="122" terrain="2,2,2,2"/>
+ <tile id="123" terrain="7,7,7,7"/>
+ <tile id="137" terrain="16,16,16,16"/>
+ <tile id="152" terrain="1,1,1,1"/>
+ <tile id="153" terrain="1,1,1,1"/>
+ <tile id="154" terrain="1,1,1,1"/>
+ <tile id="155" terrain="6,6,6,6"/>
+ <tile id="156" terrain="11,11,11,11"/>
+ <tile id="157" terrain="11,11,11,11"/>
+ <tile id="158" terrain="11,11,11,11"/>
+ <tile id="168" terrain="1,1,1,1"/>
+ <tile id="169" terrain="1,1,1,1"/>
+ <tile id="170" terrain="1,1,1,1"/>
+ <tile id="171" terrain="6,6,6,6"/>
+ <tile id="184" terrain="1,1,1,1"/>
+ <tile id="185" terrain="1,1,1,1"/>
+ <tile id="186" terrain="1,1,1,1"/>
+ <tile id="187" terrain="6,6,6,6"/>
+</tileset>
