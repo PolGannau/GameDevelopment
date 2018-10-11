7f0bb14e3c08f86e3aee71818ca19938123873bf
Pol Gannau Coma <PolGannau@users.github.com>
2018-10-11 12:21:26 +0200

    Map2
    Stage 2 terrain finnished

--- ASSETS/gameassets/sky1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="sky1" tilewidth="32" tileheight="32" tilecount="2220" columns="60">
- <image source="sky1.png" width="1920" height="1200"/>
-</tileset>
