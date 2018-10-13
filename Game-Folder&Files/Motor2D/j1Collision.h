02700860740276ef2b3b49d1ce3803d8b5239814
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2018-10-13 17:21:15 +0200

    More Collision Module

--- Game-Folder&Files/Motor2D/j1Collision.h
+++ Game-Folder&Files/Motor2D/j1Collision.h
@@ -1,1 +1,57 @@
-#pragma once
+#ifndef _j1COLLISION_H_
+#define _j1COLLISION_H_
+
+#define MAX_COLLIDERS 500
+
+#include "j1Module.h"
+#include "SDL/include/SDL_rect.h"
+
+enum COLLIDER_TYPE
+{
+	COLLIDER_NONE = -1,
+	COLLIDER_PLATFORM,
+	COLLIDER_PLAYER,
+	COLLIDER_GROUND,
+	COLLIDER_LIMIT,
+};
+
+struct Collider
+{
+	SDL_Rect			rect;
+	bool				to_delete = false;
+	COLLIDER_TYPE		type = COLLIDER_NONE;
+	j1Module*			callback = nullptr;
+	bool				enable = true;
+
+	Collider(SDL_Rect rectangle, COLLIDER_TYPE type, j1Module* callback = nullptr) :
+		rect(rectangle),
+		type(type),
+		callback(callback)
+	{}
+
+	void SetPos(int x, int y)
+	{
+		rect.x = x;
+		rect.y = y;
+	}
+
+	void SetShape(int w, int h)
+	{
+		rect.w = w;
+		rect.h = h;
+	}
+
+	void SetType(COLLIDER_TYPE type)
+	{
+		this->type = type;
+	}
+	bool CheckCollision(const SDL_Rect& r) const;
+
+	class j1Collision : public j1Module
+	{
+	private:
+		Collider* colliders[MAX_COLLIDERS];
+	};
+};
+
+#endif // !_j1COLLISION_H_
